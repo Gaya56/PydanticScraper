832745162d09187e35e6863ee65fde1504144d23
Gaya <gayahockmess@gmail.com>
2025-05-30 18:08:25 +0000

    commit all changes

---
+++ app.py
@@ -0,0 +1,4 @@
+import os
+import asyncio
+from pydantic_ai import Agent
+from pydantic_ai.mcp import MCPServerStdio
---
+++ exa_search.py
@@ -0,0 +1,5 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+import os
+from exa_py import Exa
+
